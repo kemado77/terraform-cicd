286123e6f9cc5a5dff901d40bd7530149bcab934
kemado77 <johnalexossa@gmail.com>
2024-05-21 11:41:24 -0500

    se adiciona definicion del modulo ecs en el main

--- locals.tf
+++ locals.tf
@@ -2,4 +2,16 @@ locals {
 
   ecr_repo_name = "ecr_repositorio"
 
+  demo_web_cluster_name        = "demo-web-cluster"
+  availability_zones           = ["us-east-1a", "us-east-1b"]
+  demo_web_task_famliy         = "demo-web-task"
+  container_port               = 80
+  demo_web_task_name           = "demo-web-task"
+  ecs_task_execution_role_name = "demo-web-task-execution-role"
+
+  application_load_balancer_name = "cc-demo-web-alb"
+  target_group_name              = "cc-demo-alb-tg"
+
+  demo_app_service_name = "cc-demo-web-service"
+
 }
--- main.tf
+++ main.tf
@@ -12,3 +12,20 @@ module "ecrRepo" {
   source = "./modules/ecr"
   ecr_repo_name = local.ecr_repo_name
 }
+
+module "ecsCluster" {
+  source = "./modules/ecs"
+
+  demo_web_cluster_name = local.demo_web_cluster_name
+  availability_zones    = local.availability_zones
+
+  demo_web_task_famliy         = local.demo_web_task_famliy
+  ecr_repo_url                 = module.ecrRepo.repository_url
+  container_port               = local.container_port
+  demo_web_task_name           = local.demo_web_task_name
+  ecs_task_execution_role_name = local.ecs_task_execution_role_name
+
+  application_load_balancer_name = local.application_load_balancer_name
+  target_group_name              = local.target_group_name
+  demo_web_service_name          = local.demo_web_service_name
+}
